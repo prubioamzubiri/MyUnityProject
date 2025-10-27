3cfa5c9d3e3d300340a8853a3d45f8a199adcae0
prubioamzubiri <prubioam@zubirimanteo.com>
2025-10-27 13:57:14 +0100

    Circulo creado

--- Assets/script/Persona.cs
+++ Assets/script/Persona.cs
@@ -5,7 +5,7 @@ public class Persona : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("Asset Creado");
     }
 
     // Update is called once per frame
